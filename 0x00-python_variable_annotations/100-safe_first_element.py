8c72ef8e4403aed5db94438875a27a7fa6ee5f22
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-07-22 18:14:02 +0000

    correct duck-typed annotation

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""correct duck-typed annotations"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """ Duck typing - first element of a sequence """
+    if lst:
+        return lst[0]
+    else:
+        return None
