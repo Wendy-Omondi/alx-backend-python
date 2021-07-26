501fc211ada0de4ccc582b57739eb0f25a4202bd
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-07-26 14:54:02 +0000

    pep8 fix

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """asynchronour coroutine - The basics of async"""
-import asyncio, random
+import asyncio
+import random
 
-
-async def wait_random(max_delay: int = 10) -> float:
+async def wait_random(max_delay: int=10) -> float:
     """returns interger argument afer waiting 1 - 10 seconds"""
     integer = random.uniform(0, max_delay)
     await asyncio.sleep(integer)
