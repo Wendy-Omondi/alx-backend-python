6be943ee3fe1558a37c3954ba5c250f8e6d39ef1
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-07-27 14:52:57 +0000

    pycode style fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """multiple coroutines at the same time with async"""
-import asyncio, random
-wait_random = __import__('0-basic_async_syntax').wait_random
+import asyncio
+import random
 from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
