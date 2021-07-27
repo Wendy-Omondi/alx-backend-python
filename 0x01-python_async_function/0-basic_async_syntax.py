ec7f7b596143a3eba1a2418ee2e50e67e2559c97
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-07-27 14:49:37 +0000

    pycode style fix

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,7 +3,8 @@
 import asyncio
 import random
 
-async def wait_random(max_delay: int=10) -> float:
+
+async def wait_random(max_delay: int = 10) -> float:
     """returns interger argument afer waiting 1 - 10 seconds"""
     integer = random.uniform(0, max_delay)
     await asyncio.sleep(integer)
