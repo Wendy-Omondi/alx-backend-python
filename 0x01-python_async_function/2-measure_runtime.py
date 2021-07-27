06a3de47b050f483c91410fda58e70fc1308e017
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-07-27 14:54:27 +0000

    pycode style fix

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Measures an approximate elapsed time."""
-import asyncio, time
+import asyncio
+import time
 from typing import List
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
