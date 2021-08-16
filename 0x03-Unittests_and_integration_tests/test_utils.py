05c31361a15ea736a75bc8d80baa85a3fcefa974
Wendy-Omondi <wendyomi4@gmail.com>
2021-08-16 19:41:15 +0000

    raise exceptions

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,3 +15,13 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, result):
         """method to test that the method returns what it is supposed to."""
         self.assertEqual(access_nested_map(nested_map, path), result)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """tests that a KeyError is raised for the following inputs"""
+        with self.assertRaises(KeyError) as error:
+            access_nested_map(nested_map, path)
+            self.assertEqual(icorrect, error.exception)
